14f18c9c6f70859125bddd49a88695113ecbadcb
Alex Biz <alex@alx.bz>
2011-10-12 13:34:07 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IVFReports::Application.config.secret_token = '8904ec31aecabaca6232e1645addff2d0b9a0321b04f3a449b825fcec95fb794ef59a069fc561cf9da9cec966b3fde428213b19900dabbd5e1bf0fa4cbea3f3c'
