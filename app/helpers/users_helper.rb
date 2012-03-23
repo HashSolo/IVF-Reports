7004343ccad7bda17e02c458d1539fe909da6a8f
Alex Biz <alex@alx.bz>
2012-03-23 17:59:58 -0400

    Using the gravitar to manage the clinic's image under the description. Without any clinic account it shows the IVF reports logo

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,11 @@ module UsersHelper
 												:class => 'gravatar',
 												:gravatar => options)
 	end
+	
+	def large_gravatar_for(user, options = {:size => 150})
+	  gravatar_image_tag(user.email.downcase,	:alt => user.name,
+	                      :style => 'display:block;margin-left:auto;margin-right:auto;',
+												:class => 'large_gravatar',
+												:gravatar => options)
+	end
 end
