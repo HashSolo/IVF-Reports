94772fa37cbdb6ae1978c93b01b5a4d79046a990
Alex Biz <alex@alx.bz>
2012-03-24 16:53:56 -0400

    Removed transit nomenclature

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Transit::Application.configure do
+IVFReports::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
